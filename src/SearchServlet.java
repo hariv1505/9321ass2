1408502daa1064da67ad0c74a328c371639c49de
Hari V <hariharen.veerakumar@gmail.com>
2014-05-09 14:00:58 +1000

    DB starting to work...

--- src/SearchServlet.java
+++ src/SearchServlet.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -74,7 +75,7 @@ public class SearchServlet extends HttpServlet {
 		long cOutToMS = cOut.getTimeInMillis();
 		BookingRequest b = new BookingRequest(cInToMS, cOutToMS, city);
 		session.setAttribute("BookingReq", b);
-		out.println(cInToMS + " " + cOutToMS);
+		System.out.println(cInToMS + " " + cOutToMS);
 		
 		out.println("<b>City: </b> " + city + "<br/>");
 		out.println("<b>Number of rooms: </b> " + numRooms + "<br/>");
@@ -96,13 +97,15 @@ public class SearchServlet extends HttpServlet {
 			Map<String, Integer> prices = new HashMap<String, Integer>();
 			Map<String, Integer> numberOfBeds = new HashMap<String, Integer>();
 			String searchQry = "SELECT * FROM ROOMS";
-			Connection con = DatabaseHandle.GetDbConnection();
+			
+			LoadDbDriver();
+			Connection con = GetDbConnection();
 			try {
 				PreparedStatement ps = con.prepareStatement(searchQry);
 				ResultSet rs = ps.executeQuery();
 				while (rs.next()) {
-					prices.put(rs.getString("TYPE"),Integer.parseInt(rs.getString("PRICE")));
-					prices.put(rs.getString("TYPE"),Integer.parseInt(rs.getString("NUMBEDS")));
+					prices.put(rs.getString("TYPE").trim(),Integer.parseInt(rs.getString("PRICE")));
+					numberOfBeds.put(rs.getString("TYPE").trim(),Integer.parseInt(rs.getString("NUMBEDS")));
 				}
 				
 				for (String type : sr.getRes().keySet()) {
@@ -152,7 +155,7 @@ public class SearchServlet extends HttpServlet {
 			}  catch (SQLException e) {
 				out.println("Cannot connect to database.");
 			}
-			DatabaseHandle.CloseDbConnection(con);
+			CloseDbConnection(con);
 		/*} else {
 			out.println("<br/>No available rooms. Please try again with a different query. Thank you, and "
 					+ "sorry for the inconvenience.<br/>");
@@ -164,6 +167,76 @@ public class SearchServlet extends HttpServlet {
 		out.println("</BODY>"); 
 		out.println("</HTML>");
 		out.close();
+	}
+	
+	public static void LoadDbDriver() {
+        // Load the driver
+		String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+		
+        try {
+            Class.forName(driver).newInstance();
+            System.out.println(driver + " loaded.");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.err.print("ClassNotFoundException: ");
+            System.err.println(e.getMessage());
+            System.out.println("\n Make sure your CLASSPATH variable " +
+                "contains %DERBY_HOME%\\lib\\derby.jar (${DERBY_HOME}/lib/derby.jar). \n");
+        } catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection GetDbConnection() {
+		
+        String connectionURL = "jdbc:derby:/home/hari/University/7thYear/COMP9321/Labs/Assignment2/WebContent/WEB-INF/9321ass2";
+        Connection conn = null;
+
+        // Start the database and set up users, then close database
+        try {
+            System.out.println("Trying to connect to " + connectionURL);
+            conn = DriverManager.getConnection(connectionURL);
+            System.out.println("Connected to database " + connectionURL);
+            return conn;
+        }catch(Exception e){
+        	System.out.println(e);
+        }
+        return null;
+		//
+	}
+		
+	public static void CloseDbConnection(Connection conn) {
+		try {// shut down the database
+            conn.close();
+            System.out.println("Closed connection");
+
+            /* In embedded mode, an application should shut down Derby.
+               Shutdown throws the XJ015 exception to confirm success. */
+            boolean gotSQLExc = false;
+            try {
+                DriverManager.getConnection("jdbc:derby:;shutdown=true");
+                DriverManager.getConnection("exit");
+            } catch (SQLException se) {
+                if ( se.getSQLState().equals("XJ015") ) {
+                    gotSQLExc = true;
+                }
+            }
+            if (!gotSQLExc) {
+                 System.out.println("Database did not shut down normally");
+            } else {
+                 System.out.println("Database shut down normally");
+            }
+
+            // force garbage collection to unload the EmbeddedDriver
+            //  so Derby can be restarted
+            System.gc();
+        } catch (Throwable e) {
+        	System.out.println(e);;
+            System.exit(1);
+        }
 	}
 
 }
